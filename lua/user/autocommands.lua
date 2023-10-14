c106e8fa32c0ebfefaa2f1d36426de9c5bc07ede
kkstas <109525588+kkstas@users.noreply.github.com>
2023-10-14 15:15:58 +0200

    markdown autocmds

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -4,3 +4,13 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
         vim.cmd "set formatoptions-=cro"
     end,
 })
+
+lvim.autocommands = {
+    {
+        "BufEnter",               -- see `:h autocmd-events`
+        {                         -- this table is passed verbatim as `opts` to `nvim_create_autocmd`
+            pattern = { "*.md" }, -- see `:h autocmd-events`
+            command = "setlocal conceallevel=2 wrap linebreak breakindent",
+        }
+    },
+}
